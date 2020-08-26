7963d77b0420348f434aa28766c70c74c5eb6680
shaozeye <609749056@qq.com>
2020-08-26 17:34:18 +0800

    添加项目文件。

---
+++ ERPAssist/Model/MainModel.cs
@@ -0,0 +1,25 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPAssist.Model
+{
+    public class MainModel:ObservableObject
+    {
+        private string pageSource;
+
+        public string PageSource
+        {
+            get { return pageSource; }
+            set
+            {
+                pageSource = value;
+                RaisePropertyChanged(nameof(PageSource));
+            }
+        }
+
+    }
+}
